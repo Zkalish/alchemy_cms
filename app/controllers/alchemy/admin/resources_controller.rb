d3e201f62dc61efb1c823fa49065062fb51266f8
Thomas von Deyen <tvdeyen@gmail.com>
2013-03-28 12:54:31 +0100

    Refactors the resource_relations feature.

--- app/controllers/alchemy/admin/resources_controller.rb
+++ app/controllers/alchemy/admin/resources_controller.rb
@@ -16,15 +16,13 @@ module Alchemy
       end
 
       def index
-        if params[:query].blank?
-          items = resource_handler.model
+        if params[:query].present?
+          items = queried_items
         else
-          search_terms = ActiveRecord::Base.sanitize("%#{params[:query]}%")
-          items = resource_handler.model.where(resource_handler.searchable_attributes.map { |attribute|
-            "`#{resource_handler.model.table_name}`.`#{attribute[:name]}` LIKE #{search_terms}"
-          }.join(" OR "))
+          items = resource_handler.model
         end
-        instance_variable_set("@#{resource_handler.resources_name}", items.page(params[:page] || 1).per(per_page_value_for_screen_size).order(sort_order))
+        items = items.page(params[:page] || 1).per(per_page_value_for_screen_size).order(sort_order)
+        instance_variable_set("@#{resource_handler.resources_name}", items)
       end
 
       def new
@@ -98,8 +96,10 @@ module Alchemy
       end
 
       # Returns a sort order for AR#sort method
+      #
       # Falls back to id, if the requested column is not a column of model.
       # Used by handles_sortable_columns
+      #
       def sort_order
         sortable_column_order do |column, direction|
           if resource_handler.model.column_names.include?(column.to_s)
@@ -109,6 +109,37 @@ module Alchemy
           end
         end
       end
+
+      # Returns an activerecord object that contains items matching params[:query]
+      #
+      # If the model contains model relations, all associations tables get queried as well.
+      #
+      def queried_items
+        query = params[:query].downcase.split(' ').join('%')
+        query = ActiveRecord::Base.sanitize("%#{query}%")
+        items = resource_handler.model.where(search_query(query))
+        if contains_searchable_relations?
+          items = items.includes(*resource_relations_names)
+        end
+        items
+      end
+
+      # Returns a search query string
+      #
+      # It queries all searchable attributes from resource model via LIKE and joins them via OR.
+      #
+      # If the attribute is a relation it builds the query for the associated table.
+      #
+      def search_query(search_terms)
+        resource_handler.searchable_attributes.map do |attribute|
+          if relation = attribute[:relation]
+            "`#{relation[:model_association].klass.table_name}`.`#{relation[:attr_method]}` LIKE #{search_terms}"
+          else
+            "`#{resource_handler.model.table_name}`.`#{attribute[:name]}` LIKE #{search_terms}"
+          end
+        end.join(" OR ")
+      end
+
     end
   end
 end
